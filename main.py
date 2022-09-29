66c00d561304f29daa265f0d38f495f7ec3b1260
wangziyannb <43493815+wangziyannb@users.noreply.github.com>
2022-09-29 18:08:09 -0400

    prefix path

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import csv
 
+# an example from https://blog.csdn.net/weixin_43317943/article/details/122094461?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-1-122094461-blog-60132224.pc_relevant_default&spm=1001.2101.3001.4242.2&utm_relevant_index=3
 PATH = "base.csv"
-MIN_FREQ = 2
+MIN_FREQ = 3
 MIN_CONF = 0.7
 
 
 class Node:
     def __init__(self, key, parent):
         self.key = key
-        self.count = 0
+        self.count = 1
         self.next = None
         # self.prev = None
         self.parent = parent
@@ -21,7 +22,10 @@ class FP_Tree:
         self.data = data
         self.root = Node(None, None)
         self.dict = {}
+        self.prefix_path = {}
         self.build()
+        self.get_prefix_path()
+        return
 
     def build(self):
         # todo
@@ -41,7 +45,6 @@ class FP_Tree:
                 else:
                     val.count += 1
                     pointer = val
-        return
 
     def add_new_node(self, node):
         val = self.dict.get(node.key)
@@ -54,6 +57,24 @@ class FP_Tree:
                 prev = cur
                 cur = cur.next
             prev.next = node
+
+    def get_prefix_path(self):
+        for i in self.dict:
+            # entry node
+            val = self.dict[i]
+            pointer = val
+            cur_prefix_path = []
+            while pointer is not None:
+                count = pointer.count
+                l = []
+                current = pointer.parent
+                while current != self.root:
+                    l.append(current.key)
+                    current = current.parent
+                result = (count, l)
+                cur_prefix_path.append(result)
+                pointer = pointer.next
+            self.prefix_path[i] = cur_prefix_path
 
 
 def rearrange_dict(dic):
@@ -102,6 +123,8 @@ def pre_processing(o):
     # print("freq counter", refined_counter)
     refined_counter = rearrange_dict(refined_counter)
     print("index of freq attrs", refined_counter)
+    # example: {'r': 4, 'z': 0, 'y': 2, 'x': 1, 't': 5, 's': 3}
+    # but the order of same value is random, which will change the structure of FP Tree
     return processed_data, refined_counter
 
 
