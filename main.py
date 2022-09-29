573d0b8866cb7ec8c3efdf5bc7df2aaffdc39564
wangziyannb <43493815+wangziyannb@users.noreply.github.com>
2022-09-29 14:30:38 -0400

    rearrange dict split code into pre_processing

--- main.py
+++ main.py
@@ -14,38 +14,67 @@ class Node:
 
 
 class FP_Tree:
-    def __init__(self, counter, data):
+    def __init__(self, data, counter):
         self.counter = counter
         self.data = data
         self.root = Node(None, None)
         self.build()
 
     def build(self):
+        # todo
         return
+
+
+def rearrange_dict(dic):
+    val = list(dic.values())
+    val.sort(reverse=True)
+    refined_dic = {}
+    for i in dic:
+        index = val.index(dic[i])
+        refined_dic[i] = index
+        val[index] = 'x'
+    return refined_dic
+
+
+def pre_processing(o):
+    data = []
+    processed_data = []
+    counter = {}
+    refined_counter = {}
+    f = csv.reader(o)
+    # header = next(f)
+    for row in f:
+        for prop in row:
+            value = counter.get(prop)
+            if value is None:
+                counter[prop] = 1
+            else:
+                counter[prop] = value + 1
+        data.append(row)
+    for key in counter:
+        value = counter[key]
+        if value >= MIN_FREQ:
+            refined_counter[key] = value
+    for i in data:
+        attr = []
+        for j in i:
+            value = refined_counter.get(j)
+            if value is not None:
+                attr.append(j)
+        if len(attr) == 0:
+            continue
+        processed_data.append(attr)
+
+    print("origin data: ", data)
+    print("freq data: ", processed_data)
+    print("origin counter: ", counter)
+    print("freq counter", refined_counter)
+    refined_counter = rearrange_dict(refined_counter)
+    print("index of freq attrs", refined_counter)
+    return processed_data, refined_counter
 
 
 if __name__ == '__main__':
     with open(PATH, "r") as o:
-        data = []
-        counter = {}
-        refined_counter = {}
-        f = csv.reader(o)
-        # header = next(f)
-        for row in f:
-            for prop in row:
-                value = counter.get(prop)
-                if value is None:
-                    counter[prop] = 1
-                else:
-                    counter[prop] = value + 1
-            data.append(row)
-        for key in counter:
-            value = counter[key]
-            if value >= MIN_FREQ:
-                refined_counter[key] = value
-
-        print(data)
-        print(counter)
-        print(refined_counter)
-
-        tree = FP_Tree(refined_counter, data)
+        d, c = pre_processing(o)
+        tree = FP_Tree(d, c)
